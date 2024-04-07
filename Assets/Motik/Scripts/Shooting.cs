035d7386acf01e13c540b82455b979ad5a424435
Motik335 <157040283+Motik335@users.noreply.github.com>
2024-04-07 19:48:32 +0300

    d

--- Assets/Motik/Scripts/Shooting.cs
+++ Assets/Motik/Scripts/Shooting.cs
@@ -7,17 +7,19 @@ public class Shooting : MonoBehaviour
     public GameObject bulletPrefab;
     public Transform firePoint;
     public float bulletSpeed = 10f;
-    public float fireRate = 1f;
-    private bool canShoot = true;
+    
     void Start()
     {
-        
+       
     }
+    
 
     // Update is called once per frame
     void Update()
     {
-       if(canShoot&&Input.GetButtonDown("Fire1"))
+        
+
+        if (Input.GetButtonDown("Fire1"))
         {
             Shoot();
         }
@@ -28,7 +30,7 @@ public class Shooting : MonoBehaviour
         
         GameObject bullet=Instantiate(bulletPrefab,firePoint.position,firePoint.rotation);
         Rigidbody rb=bullet .GetComponent<Rigidbody>();
-        rb.AddForce(firePoint.forward * bulletSpeed, ForceMode.Impulse);
+        rb.velocity = firePoint.up * bulletSpeed;
         
         
     }
--- Assets/Move.cs
+++ Assets/Move.cs
@@ -6,12 +6,20 @@ using UnityEngine;
 public class Move : MonoBehaviour
 {
     public float speed = 15f;
+    public float delay = 10f;
 
-    
-  
+
+
     void Start()
     {
-       
+        StartCoroutine(DestroyAfterDelay(delay));
+    }
+
+    IEnumerator DestroyAfterDelay(float delay)
+    {
+        yield return new WaitForSeconds(delay);
+
+        Destroy(gameObject);
     }
 
     // Update is called once per frame
