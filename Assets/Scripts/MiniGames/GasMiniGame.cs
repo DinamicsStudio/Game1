0410d298f41ca85929efdfbd41723fe511bd12d9
ZanY3 <146362968+ZanY3@users.noreply.github.com>
2024-04-16 17:28:07 +0300

    начало жарки(хуевое)

--- Assets/Scripts/MiniGames/GasMiniGame.cs
+++ Assets/Scripts/MiniGames/GasMiniGame.cs
@@ -1,18 +1,81 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Xml.Linq;
+using Unity.VisualScripting;
 using UnityEngine;
+using static UnityEngine.GraphicsBuffer;
 
 public class GasMiniGame : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [SerializeField] private GameObject _miniGameUI;
+    [SerializeField] private float _distanceForDrag;
+    //public Transform pan;
+    //public float maxDistanceFromPan; //îò ñêîâîðîäêè äî ìÿñà
+    //public float fryingTime; //âðåìÿ æàðêè
+    private bool _usable;
+    private bool inGame;
+    private bool isFrying = false;
+    [SerializeField] private Inventory _inventory;
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        if (Array.IndexOf(_inventory.ItemID, 1) != -1 && Input.GetKeyDown(KeyCode.E) && _usable)
+        {
+            if(!isFrying) _inventory.ObjCountInSlot[Array.IndexOf(_inventory.ItemID, 1)] = 0;
+            _usable = false;
+            inGame = true;
+            Player.canmove = false;
+            _miniGameUI.SetActive(true);
+
+        }
+        if(Input.GetKeyDown(KeyCode.Escape))
+        {
+            inGame = false;
+            Player.canmove = true;
+            _miniGameUI.SetActive(false);
+        }
+    }
+    public void Draging(Transform obj)
+    {
+        if (Vector3.Distance(obj.position, Input.mousePosition) <= _distanceForDrag)
+        {
+            obj.position = Input.mousePosition;
+        }
+    }
+    public void EndDrag(Transform obj)
+    {
+        /*
+        ÍÀÏÈÑÀÒÜ ÍÎÐÌ, ÝÒÎ ÕÓÉÍß ÊÀÊ ÁÓÄÒÎ
+        float distance = Vector3.Distance(transform.position, pan.position);
+        if (distance < maxDistanceFromPan)
+        {
+            isFrying = true
+            Debug.Log("Meat is ready!");
+            fryingTime -= Time.deltaTime;
+            if (fryingTime <= 0)
+            {
+                inGame = false;
+                Player.canmove = true;
+                _miniGameUI.SetActive(false);
+                isFrying = false
+       
+            }
+        }
+        */
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            _usable = true;
+        }
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            _usable = false;
+        }
     }
 }
