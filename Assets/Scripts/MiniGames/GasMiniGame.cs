067d1ad743af1c4d93959dc594a9712d82c55cb8
ZanY3 <146362968+ZanY3@users.noreply.github.com>
2024-04-17 19:30:10 +0300

    сделал сгорание мяса, и его выкидывание

--- Assets/Scripts/MiniGames/GasMiniGame.cs
+++ Assets/Scripts/MiniGames/GasMiniGame.cs
@@ -1,44 +1,55 @@
 using System;
+using System.Diagnostics;
+using System.Net.Http.Headers;
 using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GasMiniGame : MonoBehaviour
 {
     [SerializeField] private GameObject _miniGameUI;
     [SerializeField] private TMP_Text fryText;
-    [SerializeField] private float fryingTime; //время жарки
-    bool isFrying = false;
+     private float fryingTime = 10; //время жарки
+    private float overCookTime = 10; //время пережарки
+    private bool isFrying = false;
+    private bool isFried = false;
 
-    [Space]
-    [Space]
+    [Space, Space]
 
     private bool _usable;
     private bool inGame;
 
-    [Space]
-    [Space]
+    [Space, Space]
 
     [SerializeField] private Inventory _inventory;
     [SerializeField] private PickUp _pickUp;
 
-    [Space]
-    [Space]
+    [Space,Space]
 
     [SerializeField] private float _distanceForDrag;
-    [SerializeField] private Transform pan;
-    [SerializeField] private float maxDistanceFromPan; //от сковородки до мяса
+    [SerializeField] private float maxDistanceFromObj; //от взаимодействующего предмета
+
+    [Space, Space]
+
+    [SerializeField] private GameObject rubbishBin;
+    [SerializeField] private RectTransform _centerOfRubbish;
     [SerializeField] private RectTransform dragingMeat; //для указа позиции
     [SerializeField] private RectTransform meatStartPos;
     [SerializeField] private RectTransform _centerOfPan;
 
-    [Space]
-    [Space]
+    [Space, Space]
 
     private int meatCount;
     public GameObject ReadyMeatImage;
     [SerializeField] private GameObject meatReadyButton;
-    [SerializeField] private GameObject meatReadyNotice; //оповещение что мясо готово
-    [SerializeField] private GameObject meatIsFryingNotice; //показывает что мясо готовится
+    [SerializeField] private GameObject[] meatNotifications; //все оповещения о состоянии мяса: [0] - meatIsFriyng; [1] - meatReady; [2] - meatIsBurnt
+
+    [Space, Space]
+
+    [SerializeField] private Image meatImage;
+    [SerializeField] private Sprite rawMeatSprite; //сырое
+    [SerializeField] private Sprite friedMeatSprite; //жаренное
+
 
     public void Draging(Transform obj)
     {
@@ -49,8 +60,8 @@ public class GasMiniGame : MonoBehaviour
     }
     public void EndDrag(Transform obj)
     {
-        float distance = Vector3.Distance(obj.position, _centerOfPan.position);
-        if (distance <= maxDistanceFromPan && isFrying == false)
+        float panDistance = Vector3.Distance(obj.position, _centerOfPan.position);
+        if (panDistance <= maxDistanceFromObj && isFrying == false)
         {
             isFrying = true;
             fryText.gameObject.SetActive(true);
@@ -58,21 +69,45 @@ public class GasMiniGame : MonoBehaviour
             _inventory.ObjCountInSlot[Array.IndexOf(_inventory.ItemID, 1)] = 0;
 
         }
+        float rubbishDistance = Vector3.Distance(obj.position, _centerOfRubbish.position);
+        if (rubbishDistance <= maxDistanceFromObj && overCookTime <= 0)
+        {
+            MeatInTrash();
+        }
     }
     private void Update()
     {
+        if (isFried)
+        {
+            overCookTime -= Time.deltaTime;
+        }
+        if(overCookTime < 0)overCookTime = 0;
         if(fryingTime < 0)fryingTime = 0;
         if(isFrying)
         {
-            meatIsFryingNotice.SetActive(true);
+            meatNotifications[0].SetActive(true);
             fryingTime -= Time.deltaTime;
             fryText.text = $"Wait, meat is frying. Left {Convert.ToInt32(fryingTime)} seconds";
         }
         if (fryingTime <= 0)
         {
-            meatIsFryingNotice.SetActive(false);
+            fryText.text = $"You have {Convert.ToInt32(overCookTime)} seconds to take meat!";
+            isFried = true;
+            meatImage.sprite = friedMeatSprite;
+            meatNotifications[0].SetActive(false);
             meatReadyButton.SetActive(true);
-            meatReadyNotice.SetActive(true);
+            meatNotifications[1].SetActive(true);
+            if (overCookTime <= 0)
+            {
+                meatNotifications[2].SetActive(true);
+                rubbishBin.SetActive(true);
+                fryText.text = "The meat was burnt";
+                isFried = true;
+                meatImage.color = Color.gray;
+                meatNotifications[0].SetActive(false);
+                meatReadyButton.SetActive(false);
+                meatNotifications[1].SetActive(false);
+            }
         }
         if (inGame == false && Array.IndexOf(_inventory.ItemID, 1) != -1 && Input.GetKeyDown(KeyCode.E) && _usable) 
         {
@@ -97,10 +132,12 @@ public class GasMiniGame : MonoBehaviour
     }
     public void MeatReady()
     {
-        fryingTime = 5;
+        fryingTime = 10;
         isFrying = false;
+        isFried = false;
         meatReadyButton.SetActive(false);
-        meatReadyNotice.SetActive(false);
+        meatNotifications[1].SetActive(false);
+        meatImage.sprite = rawMeatSprite;
         for (int i = 0; i < meatCount; i++)
         {
             _pickUp.PickUping(12, 2, _inventory, ReadyMeatImage, false);
@@ -110,6 +147,26 @@ public class GasMiniGame : MonoBehaviour
         _miniGameUI.SetActive(false);
         dragingMeat.position = meatStartPos.position;
         fryText.gameObject.SetActive(false);
+    }
+    public void MeatInTrash()
+    {
+        overCookTime = 10;
+        fryingTime = 10;
+        isFrying = false;
+        isFried = false;
+        meatReadyButton.SetActive(false);
+        meatNotifications[1].SetActive(false);
+        meatNotifications[2].SetActive(false);
+        meatImage.sprite = rawMeatSprite;
+        inGame = false;
+        Player.canmove = true;
+        rubbishBin.SetActive(true);
+        dragingMeat.position = meatStartPos.position;
+        fryText.gameObject.SetActive(false);
+        meatImage.color = Color.white;
+        rubbishBin.SetActive(false);
+        _miniGameUI.SetActive(false);
+
     }
 
     private void OnTriggerEnter(Collider other)
